8a73f736a5b48fdd534af5cbc4c219132b4a0c1b
ankitsrivastava777 <ankit@excellencetechnologies.info>
2020-03-12 13:29:42 +0530

    unnecessary file deleted

--- Excellence/Slider/Model/ResourceModel/Slides.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Excellence\Slider\Model\ResourceModel;
-class Slides extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
-{
-    protected function _construct()
-    {
-        $this->_init('excellence_slider_slides','id');
-    }
-}
--- Excellence/Slider/Model/SliderpagesInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Excellence\Slider\Model;
-interface SliderpagesInterface 
-{
-
-}
